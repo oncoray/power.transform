0ff0635444faf367e04ef51d85c672f308002dc9
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2022-12-15 13:03:50 +0100

    Initial commit

---
+++ R/power.transform-package.R
@@ -0,0 +1,17 @@
+#' @docType package
+#' @name power.transform
+#' @import methods
+#' @importFrom data.table data.table
+#' @importFrom data.table :=
+#' @importFrom data.table .SD
+#' @importFrom data.table .BY
+#' @importFrom data.table .N
+#' @importFrom data.table .I
+#' @importFrom data.table .GRP
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .EACHI
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# power.transform
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of power.transform is to ...
+
+## Installation
+
+You can install the development version of power.transform like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(power.transform)
+## basic example code
+```
+
