dd5d7bd2065cc0665fb61c12d9023f75d1ade2ad
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-01-04 13:38:32 +0100

    Added packages for plotting.

--- R/power.transform-package.R
+++ R/power.transform-package.R
@@ -1,13 +1,14 @@
-#' power.transform: Power-transformations for Transforming Feature Distributions
-#' to Normality
+#' power.transform: Transform Data to Normality using Power Transformations
 #'
-#' This package is based on, and contains code from, the familiar package
-#' (<https://cran.r-project.org/package=familiar>), under the EUPL license.
+#' This package was originally based on, and contains code from, the familiar
+#' package (<https://cran.r-project.org/package=familiar>), under the EUPL
+#' license.
 #'
 #' @docType package
 #' @name power.transform
 #' @import data.table
 #' @import methods
+#' @importFrom rlang .data
 "_PACKAGE"
 
 ## usethis namespace: start
