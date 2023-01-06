cc137da759d06d7f0392757dd2139454d230ddea
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-01-06 17:17:30 +0100

    Added check on lambda-range.

--- R/Checks.R
+++ R/Checks.R
@@ -37,3 +37,34 @@
 
   return(invisible(TRUE))
 }
+
+
+
+.check_lambda_range <- function(x){
+
+  # NULL is a valid value.
+  if(is.null(x)) return(invisible(TRUE))
+
+  # Otherwise, must be length 2, numeric, finite and sorted.
+  if(length(x) != 2){
+    stop(paste0("lambda_range should consist of 2 numeric values. ", length(x), " values were found."))
+  }
+
+  if(!is.numeric(x)){
+    stop("lambda_range should consist of 2 numeric values. The values are not numeric.")
+  }
+
+  if(any(!is.finite(x))){
+    stop("lambda_range should consist of 2 numeric values. One or both values are not finite.")
+  }
+
+  if(diff(x) == 0.0){
+    stop("lambda_range should consist of 2 numeric values. The values can not be identical.")
+  }
+
+  if(is.unsorted(x)){
+    stop("lambda_range should consist of 2 numeric values. The values are not sorted by increasing value.")
+  }
+
+  return(invisible(TRUE))
+}
