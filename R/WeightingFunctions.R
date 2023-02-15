923f71fe1d4333b26e7862e81705a1747e94fa91
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-02-15 16:48:48 +0100

    Implemented .get_weights method for weighting methods.

--- R/WeightingFunctions.R
+++ R/WeightingFunctions.R
@@ -61,228 +61,229 @@ setClass("weightingMethodResidualCosine", contains=c("weightingSourceResidual", 
 
 
 
+# .get_weights (general weighting) ---------------------------------------------
+setMethod(
+  ".get_weights",
+  signature(object = "weightingFunctionGeneric"),
+  function(object, x, ...){
+    # Weights are set by first processing the data, and then applying the
+    # weight function to the processed input data.
 
-original_step <- function(x, k1=0.80, ...){
-  return(.step_window(
-    x = .compute_weight_original(x = x),
-    k1 = k1))
-}
+    y <- .compute_weight_input(
+      object = object,
+      x = x)
 
-original_triangle <- function(x, k1=0.70, k2=0.90, ...){
-  return(.triangular_window(
-    x = .compute_weight_original(x = x),
-    k1 = k1,
-    k2 = k2))
-}
+    w <- .apply_weight_function(
+      object = object,
+      x = y)
 
-original_cosine <- function(x, k1=0.70, k2=0.90, ...){
-  return(.tapered_cosine_window(
-    x = .compute_weight_original(x = x),
-    k1 = k1,
-    k2 = k2))
-}
-
-transformed_step <- function(x, lambda, type, k1=stats::qnorm(0.90), ...){
-  return(.step_window(
-    x = .compute_weight_transformed(
-      x = x,
-      lambda = lambda,
-      type = type),
-    k1 = k1))
-}
-
-transformed_triangle <- function(x, lambda, type, k1=stats::qnorm(0.85), k2=stats::qnorm(0.95), ...){
-  return(.triangular_window(
-    x = .compute_weight_transformed(
-      x = x,
-      lambda = lambda,
-      type = type),
-    k1 = k1,
-    k2 = k2))
-}
-
-transformed_cosine <- function(x, lambda, type, k1=stats::qnorm(0.85), k2=stats::qnorm(0.95), ...){
-  return(.tapered_cosine_window(
-    x = .compute_weight_transformed(
-      x = x,
-      lambda = lambda,
-      type = type),
-    k1 = k1,
-    k2 = k2))
-}
-
-residual_step <- function(x, lambda, type, k1=0.40, ...){
-  return(.step_window(
-    x = .compute_weight_residual(
-      x = x,
-      lambda = lambda,
-      type = type),
-    k1 = k1))
-}
-
-residual_triangle <- function(x, lambda, type, k1=0.30, k2=0.50, ...){
-  return(.triangular_window(
-    x = .compute_weight_residual(
-      x = x,
-      lambda = lambda,
-      type = type),
-    k1 = k1,
-    k2 = k2))
-}
-
-residual_cosine <- function(x, lambda, type, k1=0.30, k2=0.50, ...){
-  return(.tapered_cosine_window(
-    x = .compute_weight_residual(
-      x = x,
-      lambda = lambda,
-      type = type),
-    k1 = k1,
-    k2 = k2))
-}
-
-
-
-.compute_weight_original <- function(x){
-  # Check if x is sorted.
-  if(is.unsorted(x)) stop(paste0("DEV: x is expected to be sorted in ascending order."))
-
-  # Compute empirical probabilities of each point
-  p <- (seq_along(x) - 1/3) / (length(x) + 1/3)
-
-  # Centralise and map to [-1, 1] range.
-  p <- 2.0 * (p - 0.5)
-
-  return(p)
-}
-
-
-
-.compute_weight_transformed <- function(x, lambda, type){
-
-  # Set transformer.
-  transform_FUN <- switch(
-    type,
-    "box_cox"=..box_cox_transform,
-    "yeo_johnson"=..yeo_johnson_transform
-  )
-
-  # Find transformed feature values.
-  y <- do.call(
-    transform_FUN,
-    args=list(
-      "lambda"=lambda,
-      "x"=x))
-
-  # Approximate Huber's M-estimates for locality and scale.
-  robust_estimates <- huber_estimate(y, tol=1E-3)
-
-  # Check problematic values.
-  if(!is.finite(robust_estimates$sigma)) return(NA_real_)
-  if(robust_estimates$sigma == 0.0) return(NA_real_)
-
-  return((y - robust_estimates$mu) / robust_estimates$sigma)
-}
-
-
-
-.compute_weight_residual <- function(x, lambda, type){
-  # Set transformation function
-  transform_FUN <- switch(
-    type,
-    "box_cox"=..box_cox_transform,
-    "yeo_johnson"=..yeo_johnson_transform
-  )
-
-  # Find transformed feature values.
-  y <- do.call(
-    transform_FUN,
-    args=list(
-      "lambda"=lambda,
-      "x"=x))
-
-  # Compute the expected z-score.
-  z_expected <- compute_expected_z(x=x)
-
-  # Approximate Huber's M-estimates for locality and scale.
-  robust_estimates <- huber_estimate(y, tol=1E-3)
-
-  # Check problematic values.
-  if(!is.finite(robust_estimates$sigma)) return(NA_real_)
-  if(robust_estimates$sigma == 0.0) return(NA_real_)
-
-  # Compute the observed z-score.
-  z_observed <- (y - robust_estimates$mu) / robust_estimates$sigma
-
-  # Compute residuals.
-  residual <- z_observed - z_expected
-
-  return(residual)
-}
-
-
-
-.step_window <- function(x, k1, ...){
-  # Set weights using a step window.
-
-  # k1 should be 0 or greater.
-  if(k1 < 0) return(NA_real_)
-
-  # Initialise weights.
-  w <- numeric(length(x)) + 1.0
-
-  # Set step window.
-  w[abs(x) > k1] <- 0.0
-
-  return(w)
-}
-
-
-
-.triangular_window <- function(x, k1, k2, ...){
-  # Set weights using a triangular window.
-
-  # k1 should be 0 or greater, and k2 cannot be smaller than k1.
-  if(k2 < k1) return(NA_real_)
-  if(k1 < 0) return(NA_real_)
-
-  # Initialise weights.
-  w <- numeric(length(x)) + 1.0
-
-  # Set weights of elements between k1 and k2.
-  if(k1 != k2){
-    lobe_elements <- which(abs(x) >= k1 & abs(x) <= k2)
-    w[lobe_elements] <- 1.0 - (abs(x[lobe_elements]) - k1) / (k2 - k1)
+    return(w)
   }
-
-  # Set weights of elements greater than k2.
-  w[abs(x) > k2] <- 0.0
-
-  return(w)
-}
+)
 
 
 
-.tapered_cosine_window <- function(x, k1, k2, ...){
-  # Set weights using a tapered cosine window.
+# .compute_weight_input (generic) ----------------------------------------------
+setGeneric(
+  ".compute_weight_input",
+  function(object, ...) standardGeneric(".compute_weight_input"))
 
-  # k1 should be 0 or greater, and k2 cannot be smaller than k1.
-  if(k2 < k1) return(NA_real_)
-  if(k1 < 0) return(NA_real_)
 
-  # Initialise weights.
-  w <- numeric(length(x)) + 1.0
 
-  # Set weights of elements between k1 and k2.
-  if(k1 != k2){
-    lobe_elements <- which(abs(x) >= k1 & abs(x) <= k2)
-    w[lobe_elements] <- 0.5 + 0.5 * cos((abs(x[lobe_elements]) - k1) / (k2 - k1) * pi)
+# .compute_weight_input (none) -------------------------------------------------
+setMethod(
+  ".compute_weight_input",
+  signature(object = "weightingSourceNone"),
+  function(object, x, ...){
+    return(x)
   }
+)
 
-  # Set weights of elements greater than k2.
-  w[abs(x) > k2] <- 0.0
 
-  return(w)
-}
+
+# .compute_weight_input (empirical probability) --------------------------------
+setMethod(
+  ".compute_weight_input",
+  signature(object = "weightingSourceEmpiricalProbability"),
+  function(object, x, ...){
+    # Check if x is sorted.
+    if(is.unsorted(x)) stop(paste0("DEV: x is expected to be sorted in ascending order."))
+
+    # Compute empirical probabilities of each point
+    p <- (seq_along(x) - 1/3) / (length(x) + 1/3)
+
+    # Centralise and map to [-1, 1] range.
+    p <- 2.0 * (p - 0.5)
+
+    return(p)
+  }
+)
+
+
+
+# .compute_weight_input (transformed) ------------------------------------------
+setMethod(
+  ".compute_weight_input",
+  signature(object = "weightingSourceTransformed"),
+  function(object, transformer, x, ...){
+    # Find transformed feature values.
+    y <- ..transform(
+      object = object,
+      x = x)
+
+    # Approximate Huber's M-estimates for locality and scale.
+    robust_estimates <- huber_estimate(y, tol=1E-3)
+
+    # Check problematic values.
+    if(!is.finite(robust_estimates$sigma)) return(NA_real_)
+    if(robust_estimates$sigma == 0.0) return(NA_real_)
+
+    return((y - robust_estimates$mu) / robust_estimates$sigma)
+  }
+)
+
+
+
+# .compute_weight_input (residual) ---------------------------------------------
+setMethod(
+  ".compute_weight_input",
+  signature(object = "weightingSourceResidual"),
+  function(object, transformer, x, ...){
+    # Find transformed feature values.
+    y <- ..transform(
+      object = object,
+      x = x)
+
+    # Compute the expected z-score.
+    z_expected <- compute_expected_z(x=x)
+
+    # Approximate Huber's M-estimates for locality and scale.
+    robust_estimates <- huber_estimate(y, tol=1E-3)
+
+    # Check problematic values.
+    if(!is.finite(robust_estimates$sigma)) return(NA_real_)
+    if(robust_estimates$sigma == 0.0) return(NA_real_)
+
+    # Compute the observed z-score.
+    z_observed <- (y - robust_estimates$mu) / robust_estimates$sigma
+
+    # Compute residuals.
+    residual <- z_observed - z_expected
+
+    return(residual)
+  }
+)
+
+
+
+# .apply_weight_function (generic) ---------------------------------------------
+setGeneric(
+  ".apply_weight_function",
+  function(object, ...) standardGeneric(".apply_weight_function"))
+
+
+
+# .apply_weight_function (none) ------------------------------------------------
+setMethod(
+  ".apply_weight_function",
+  signature(object = "weightingFunctionNone"),
+  function(object, x, ...){
+    # All weights are 1.0.
+    w <- rep_len(1.0, length(x))
+
+    return(w)
+  }
+)
+
+
+
+# .apply_weight_function (step) ------------------------------------------------
+setMethod(
+  ".apply_weight_function",
+  signature(object = "weightingFunctionStep"),
+  function(object, x, ...){
+    # Set weights using a step window.
+
+    # k1 should be 0 or greater.
+    if(object@k1 < 0.0) return(NA_real_)
+
+    # Initialise weights.
+    w <- rep_len(1.0, length(x))
+
+    # Set step window.
+    w[abs(x) > object@k1] <- 0.0
+
+    return(w)
+  }
+)
+
+
+
+# .apply_weight_function (triangular) ------------------------------------------
+setMethod(
+  ".apply_weight_function",
+  signature(object = "weightingFunctionTriangle"),
+  function(object, x, ...){
+    # Set weights using a triangular window.
+
+    # k1 should be 0 or greater, and k2 cannot be smaller than k1.
+    if(object@k2 < object@k1) return(NA_real_)
+    if(object@k1 < 0.0) return(NA_real_)
+
+    # Initialise weights.
+    w <- rep_len(1.0, length(x))
+
+    # Set weights of elements between k1 and k2.
+    if(object@k1 != object@k2){
+      lobe_elements <- which(abs(x) >= object@k1 & abs(x) <= object@k2)
+      w[lobe_elements] <- 1.0 - (abs(x[lobe_elements]) - object@k1) / (object@k2 - object@k1)
+    }
+
+    # Set weights of elements greater than k2.
+    w[abs(x) > object@k2] <- 0.0
+
+    return(w)
+  }
+)
+
+
+
+# .apply_weight_function (tapered cosine) --------------------------------------
+setMethod(
+  ".apply_weight_function",
+  signature(object = "weightingFunctionCosine"),
+  function(object, x, ...){
+    # Set weights using a tapered cosine window.
+
+    # k1 should be 0 or greater, and k2 cannot be smaller than k1.
+    if(object@k2 < object@k1) return(NA_real_)
+    if(object@k1 < 0.0) return(NA_real_)
+
+    # Initialise weights.
+    w <- rep_len(1.0, length(x))
+
+    # Set weights of elements between k1 and k2.
+    if(object@k1 != object@k2){
+      lobe_elements <- which(abs(x) >= object@k1 & abs(x) <= object@k2)
+      w[lobe_elements] <- 0.5 + 0.5 * cos((abs(x[lobe_elements]) - object@k1) / (object@k2 - object@k1) * pi)
+    }
+
+    # Set weights of elements greater than k2.
+    w[abs(x) > object@k2] <- 0.0
+
+    return(w)
+  }
+)
+
+
+
+# ..get_default_weighting_parameters (generic) ---------------------------------
+setGeneric(
+  "..get_default_weighting_parameters",
+  function(object, ...) standardGeneric("..get_default_weighting_parameters"))
+
+
+
 
 
 
