89fd3b9341422daba5d1ad8b49a2fbf5859f9f99
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-02-15 16:46:28 +0100

    Added default warning message for poor weighting methods.

---
+++ R/DefaultConditionSignals.R
@@ -0,0 +1,12 @@
+.warn_poor_weighting_method <- function(x){
+
+  # Throw a warning.
+  rlang::warn(
+    message = paste0(
+      "The ", x, " weighting function leads to worse overall parameter estimations than non-robust transformations ",
+      "and should be avoided. Use one of the weighting functions based on empirical probability instead."),
+    .frequency = "once"
+  )
+
+  return(invisible(TRUE))
+}
