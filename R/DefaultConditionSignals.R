69760c785cac48c3725d14cf7c9cf60b51a2d151
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-02-23 11:45:44 +0100

    Added id.

--- R/DefaultConditionSignals.R
+++ R/DefaultConditionSignals.R
@@ -5,7 +5,8 @@
     message = paste0(
       "The ", x, " weighting function leads to worse overall parameter estimations than non-robust transformations ",
       "and should be avoided. Use one of the weighting functions based on empirical probability instead."),
-    .frequency = "once"
+    .frequency = "once",
+    .frequency_id = "poor_weight_method"
   )
 
   return(invisible(TRUE))
