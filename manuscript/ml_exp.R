289304ea306734e1c3aec1a9271b8e758910ddeb
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2023-08-08 14:57:45 +0200

    Update to ml_exp.R for experiment with offsets on numeric values.

--- manuscript/ml_exp.R
+++ manuscript/ml_exp.R
@@ -1,5 +1,3 @@
-experiment_dir <- r"(C:\Users\alexz\Documents\GitHub\power.transform\manuscript\ml_experiment)"
-
 configurations <- list(
   "config_no_transformation_glm" = list(
     "transformation_method" = "none",
@@ -29,6 +27,24 @@ configurations <- list(
   )
 )
 
+# Experiment without offset ----------------------------------------------------
+
+# experiment_dir <- r"(C:\Users\alexz\Documents\GitHub\power.transform\manuscript\ml_experiment)"
+#
+# results <- familiar.experiment::run_experiment(
+#   experiment_dir = experiment_dir,
+#   experiment_configs = configurations,
+#   fixed_familiar_parameters = list(
+#     "fs_method" = "mim"
+#   ),
+#   n_repetitions = 5L,
+#   n_nodes = 18L
+# )
+
+# Experiment with offset -------------------------------------------------------
+
+experiment_dir <- r"(C:\Users\alexz\Documents\GitHub\power.transform\manuscript\ml_experiment_offset)"
+
 results <- familiar.experiment::run_experiment(
   experiment_dir = experiment_dir,
   experiment_configs = configurations,
@@ -36,21 +52,6 @@ results <- familiar.experiment::run_experiment(
     "fs_method" = "mim"
   ),
   n_repetitions = 5L,
+  numeric_offset = 1E4,
   n_nodes = 18L
 )
-
-results[, "experiment_parameters" := sub(pattern = "_glm", replacement = "", x = experiment_parameters)]
-results[, "experiment_parameters" := sub(pattern = "_rf", replacement = "", x = experiment_parameters)]
-
-results <- data.table::dcast(
-  data = results,
-  dataset + iteration_id + learner + dataset_split + metric + outcome_type ~ experiment_parameters,
-  value.var = "value")
-
-results <- results[dataset_split == "test"]
-
-results[, list("improved" = familiar.experiment::assess_improvement(x = config_new_test, y = config_no_transformation, metric = metric)), by = c("dataset", "iteration_id", "learner")][, list(n = .N), by = c("improved", "learner")][order(improved, learner)]
-
-wilc_data <- results[, list("improved" = familiar.experiment::assess_improvement(x = config_new_test, y = config_no_transformation, metric = metric)), by = c("dataset", "iteration_id", "learner")]
-wilcox.test(x=as.numeric(wilc_data$improved) - 2.0, alternative = "greater")
-ggplot2::ggplot(data = wilc_data, mapping = ggplot2::aes(x = improved)) + ggplot2::geom_bar()
