1e1f08d5c5672d3c41e8ec9f80e863cf4abe68cb
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-02-19 19:51:10 +0100

    Add xgboost_lm as learner type.

--- manuscript_rmarkdown/ml_exp.R
+++ manuscript_rmarkdown/ml_exp.R
@@ -74,12 +74,50 @@ configurations <- list(
     "transformation_method" = "yeo_johnson_conventional",
     "learner" = "random_forest_ranger",
     "normalisation_method" = "none"
+  ),
+  "config_no_transformation_xgboost" = list(
+    "transformation_method" = "none",
+    "learner" = "xgboost_lm"
+  ),
+  "config_no_transformation_xgboost_no_normalisation" = list(
+    "transformation_method" = "none",
+    "learner" = "xgboost_lm",
+    "normalisation_method" = "none"
+  ),
+  "config_invariant_robust_xgboost" = list(
+    "transformation_method" = "yeo_johnson_robust",
+    "learner" = "xgboost_lm"
+  ),
+  "config_invariant_robust_xgboost_no_normalisation" = list(
+    "transformation_method" = "yeo_johnson_robust",
+    "learner" = "xgboost_lm",
+    "normalisation_method" = "none"
+  ),
+  "config_invariant_robust_gof_xgboost" = list(
+    "transformation_method" = "yeo_johnson_robust",
+    "transformation_gof_test_p_value" = 0.01,
+    "learner" = "xgboost_lm"
+  ),
+  "config_invariant_robust_gof_xgboost_no_normalisation" = list(
+    "transformation_method" = "yeo_johnson_robust",
+    "transformation_gof_test_p_value" = 0.01,
+    "learner" = "xgboost_lm",
+    "normalisation_method" = "none"
+  ),
+  "config_conventional_xgboost" = list(
+    "transformation_method" = "yeo_johnson_conventional",
+    "learner" = "xgboost_lm"
+  ),
+  "config_conventional_xgboost_no_normalisation" = list(
+    "transformation_method" = "yeo_johnson_conventional",
+    "learner" = "xgboost_lm",
+    "normalisation_method" = "none"
   )
 )
 
 # Experiment without offset ----------------------------------------------------
 
-experiment_dir <- r"(C:\Users\alexz\Documents\GitHub\power.transform\manuscript\ml_experiment)"
+experiment_dir <- r"(C:\Users\alexz\Documents\GitHub\power.transform\manuscript_rmarkdown\ml_experiment)"
 
 results <- familiar.experiment::run_experiment(
   experiment_dir = experiment_dir,
