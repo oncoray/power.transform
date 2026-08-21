d8049fd193e767b97e55646c7925342f9ef062be
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-08-21 13:42:36 +0200

    Add thread limiter to testthat.R.

--- R/tests/testthat.R
+++ R/tests/testthat.R
@@ -1,12 +1,7 @@
-# This file is part of the standard setup for testthat.
-# It is recommended that you do not modify it.
-#
-# Where should you do additional test configuration?
-# Learn more about the roles of various files in:
-# * https://r-pkgs.org/tests.html
-# * https://testthat.r-lib.org/reference/test_package.html#special-files
-
 library(testthat)
 library(power.transform)
 
-test_check("power.transform")
+# Prevent thread overuse (through data.table?) when running tests on CRAN.
+Sys.setenv("OMP_THREAD_LIMIT" = 2L)
+
+testthat::test_check("power.transform")
