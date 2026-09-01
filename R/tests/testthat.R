b9376de719545a7f308b810abe4a8200bf5aad93
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-09-01 08:38:43 +0200

    Update testthat.R

--- R/tests/testthat.R
+++ R/tests/testthat.R
@@ -1,7 +1,7 @@
-library(testthat)
-library(power.transform)
-
 # Prevent thread overuse (through data.table?) when running tests on CRAN.
 Sys.setenv("OMP_THREAD_LIMIT" = 2L)
+
+library(testthat)
+library(power.transform)
 
 testthat::test_check("power.transform")
